ed75ecfb2aaaf3970ab3a401c0813f76ede4d090
Saksham Gupta <95184568+26Saksham@users.noreply.github.com>
2023-03-23 22:46:22 +0530

    Add files via upload
    changes

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import connection from './database/db.js';
 import Router from './Routes/Routes.js'
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 const app = express()
 // console.log("MongoDB database connection established successfully.");
 // })
@@ -14,7 +16,7 @@ app.use(cors({origin: true, credentials: true}));
 app.use(bodyParser.json({extended:false}));
 app.use(bodyParser.urlencoded({extended:false}))
 app.use('/',Router);
-const port=8000;
+const port=process.env.PORT||8000;
 
 app.listen(port,()=>console.log('server is started at port no.'+port));
 
