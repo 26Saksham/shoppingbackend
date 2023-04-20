34b1ab71026cf84081538bb92c570bff37d9e6ee
Saksham Gupta <95184568+26Saksham@users.noreply.github.com>
2023-04-20 23:46:13 +0530

    second chages
    second change

--- index.js
+++ index.js
@@ -5,17 +5,28 @@ import connection from './database/db.js';
 import Router from './Routes/Routes.js'
 import bodyParser from 'body-parser';
 import cors from 'cors';
+
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+
 dotenv.config();
+
 const app = express()
-// console.log("MongoDB database connection established successfully.");
-// })
+
     
 
 app.use(cors({origin: true, credentials: true}));
 app.use(bodyParser.json({extended:false}));
 app.use(bodyParser.urlencoded({extended:false}))
 app.use('/',Router);
+app.use(express.json());
+app.use(cors({
+    origin:"http://localhost:3000"
+}))
+
+
+
+
+
 const port=process.env.PORT||8000;
 
 app.listen(port,()=>console.log('server is started at port no.'+port));
